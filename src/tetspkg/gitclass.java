7b36a20bec88113ece08b5c68b654b804ed0c34e
Mahesh <maheshbabuece143@gmail.com>
2023-03-21 03:55:06 -0600

    Update

--- src/tetspkg/gitclass.java
+++ src/tetspkg/gitclass.java
@@ -5,7 +5,8 @@ public class gitclass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("new One");
-
+		System.out.println("eewee");
+         
 	}
 
 }
